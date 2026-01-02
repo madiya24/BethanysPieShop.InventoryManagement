e1580cb06154b07fb55201a7f73fefdf05408846
Chrines <chrinesskatete@gmail.com>
2026-01-02 12:49:44 +0200

    Declaration of variable and adding an UnitType class

---
+++ Product.cs
@@ -0,0 +1,10 @@
+namespace BethanysPieShop.InventoryManagement
+{
+    public class Product
+    {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+        private int maxItemsInStock = 0;
+    }
+}
