0ec5c0743a0d38c268738ccd19a41ac0941ca612
Chrines <chrinesskatete@gmail.com>
2026-01-02 16:37:32 +0200

    Split procu

--- Domain/ProductManagement/Product.cs
+++ Domain/ProductManagement/Product.cs
@@ -6,7 +6,7 @@ using BethanysPieShop.InventoryManagement.General; // for async programming
 
 namespace BethanysPieShop.InventoryManagement.ProductManagement
 {
-    public class Product
+    public partial class Product
     {
         private int id;
         private string name = string.Empty; // initialize to empty string
@@ -76,121 +76,6 @@ namespace BethanysPieShop.InventoryManagement.ProductManagement
             maxItemsInStock = maxAmountInStock;
 
             UpdateLowStock();
-        }
-
-        
-
-        public void UseProduct(int items)
-        {
-            if(items <= AmountInStock)
-            {
-                AmountInStock -= items;
-                UpdateLowStock();
-
-                Log($"Amount in stock updated. Now {AmountInStock} items in stock.");
-            }
-            else
-            {
-                Log($"Not enough items in stock for {CreateSimpleProductRepresentation()}. {AmountInStock} available, {items} requested.");
-            }
-
-        }
-
-        public void IncreaseStock()
-        {
-            AmountInStock++;
-        }
-
-        public void IncreaseStock(int amount)
-        {
-            int newStock = AmountInStock + amount;
-
-            if(newStock <= maxItemsInStock)
-            {
-                AmountInStock += amount;
-            }
-            else
-            {
-                AmountInStock = maxItemsInStock; //we only store the possible maximum items
-                Log($"Created {CreateSimpleProductRepresentation()}. Stock overflow {newStock - AmountInStock} item(s) ordered that could'nt be stored..");
-            }
-
-            if(AmountInStock > 10)
-            {
-                IsBelowStockThreshold = false;
-            }
-        }
-
-
-        public void DecreaseStock(int items, string reason)
-        {
-            if(maxItemsInStock <= AmountInStock)
-            {
-                //decrease stock with the specified number of items
-                AmountInStock -= items;
-            }
-            else
-            {
-                AmountInStock = 0;
-            }
-            UpdateLowStock();
-        }
-
-        public string DisplayDetailsShort()
-        {
-            return $"{id}. {name} \n{AmountInStock} items in stock.";
-        }
-
-        public string DisplayDetailsFull()
-        {
-
-           StringBuilder sb = new();
-
-            sb.Append($"{Id}: {name} \n{description}\n{Price}\n{AmountInStock} item(s) in stock.");
-
-            if(IsBelowStockThreshold)
-           {
-             sb.Append("\n!!STOCK LOW!!");
-          }
-            return sb.ToString();
-            //return DisplayDetailsFull("");
-        }
-
-        public string DisplayDetailsFull(string extraDetails)
-        {
-
-            StringBuilder sb = new StringBuilder();
-
-            sb.Append($"{Id}: {name} \n{description}\n{Price}\n{AmountInStock} item(s) in stock.");
-            sb.Append($"\n{extraDetails}");
-
-            if(IsBelowStockThreshold)
-            {
-                sb.Append("\n!!STOCK LOW!!");
-            }
-            return sb.ToString();
-            
-        }
-
-        public void UpdateLowStock()
-        {
-            if(AmountInStock < 10) // for max value
-            {
-                IsBelowStockThreshold = true;
-                
-            }
-            
-        }
-
-        public void Log(string message)
-        {
-            //this coild be written to a file or database instead
-            Console.WriteLine(message);
-        }
-
-        public string CreateSimpleProductRepresentation()
-        {
-            return $"Product: {id} ({name})";
         }
     }
 }
